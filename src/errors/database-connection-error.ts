748a7c7ede46b7cd71952b0e1bb7f048aee365a5
clarklindev <clarklindev@gmail.com>
2024-12-04 12:52:03 +0800

    updates

---
+++ src/errors/database-connection-error.ts
@@ -0,0 +1,24 @@
+//database-connection-error.ts
+import { CustomError } from './custom-error';
+
+export class DatabaseConnectionError extends CustomError {
+  reason = 'Error connecting to database';
+  statusCode = 500;
+
+  constructor() {
+    super('Error connecting to db');
+
+    //only because we are extending a built in class
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [
+      {
+        message: this.reason,
+      },
+    ];
+  }
+}
+
+//usage: throw new DatabaseConnectionError();
